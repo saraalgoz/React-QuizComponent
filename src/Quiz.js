fc21e0e38b02eb815063399193ee6c416a585f51
saraalgoz <sara.algoz@gmail.com>
2019-02-19 21:37:38 +0100

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component{
     this.state = { quiz_position: 1 }
   }
   render(){
-    const isQuizEnd = true
+    const isQuizEnd = false
     //((this.state.quiz_position -1) ===quizData.quiz_questions.length)
     return (
       <div>
